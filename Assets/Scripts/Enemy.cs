63d1485766e30405fa845fc304ef665c075ef525
Hardcore5412 <115845218+Hardcore5412@users.noreply.github.com>
2023-05-27 22:53:18 -0500

    First COmmmit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int Health;
+    public Transform target;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Animator animator;
+    private float moveX;
+    private float moveY;
+    private bool isPunch;
+    private float speed;
+    
+    
+    void Start()
+    {
+        gameObject.tag = "Player";
+    }
+
+    
+    void Update()
+    {
+        
+        transform.position += new Vector3(moveX,moveY,0) * speed * Time.deltaTime;
+        animator.SetFloat("Speed", Mathf.Sqrt( Mathf.Pow(moveX,2) + Mathf.Pow(moveY,2)));
+        
+    }
+    void OnMove(InputValue movementvalue){
+        Vector2 moveVector = movementvalue.Get<Vector2>();
+        moveX = moveVector.x;
+        moveY = moveVector.y;
+       
+        if(moveVector.x > 0.0f){
+            gameObject.transform.rotation = Quaternion.Euler(0,0,0);
+        }
+        else if(moveVector.x < 0.0f){
+            gameObject.transform.rotation = Quaternion.Euler(0,180,0);
+        }
+
+    }
+    void OnJab(InputValue Jab){
+        animator.SetBool("IsPunching", true);
+        speed = 0.1f;
+    }
+    public void EndJab(){
+        animator.SetBool("IsPunching", false);
+        speed = 1.0f;
+    }
+}
